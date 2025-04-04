79c12430a109630d75bbae3589ad195ae683bd19
rdas1 <lilmessi42@gmail.com>
2025-04-04 00:58:58 -0400

    adjusted cat

--- frontend/src/components/SleepyFooter.tsx
+++ frontend/src/components/SleepyFooter.tsx
@@ -3,7 +3,7 @@ import sleepingCat from "@/assets/sleeping_cat.json";
 
 export default function SleepyFooter() {
   return (
-    <div className="w-full mt-6 flex justify-center items-end gap-12 pb-6">
+    <div className="w-full flex justify-center items-end gap-12 pb-2">
       <Lottie animationData={sleepingCat} loop={true} className="w-28 h-28" />
     </div>
     
